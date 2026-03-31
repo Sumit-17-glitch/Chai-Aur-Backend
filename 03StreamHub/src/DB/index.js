acfdaaf78008d828a853b83b3089d6dd9255b6e4
Sumit-17-glitch <skkillgrave@gmail.com>
2026-03-31 21:41:36 +0530

    database connection

--- 03StreamHub/src/DB/index.js
+++ 03StreamHub/src/DB/index.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const dbConnect = async () => {
     try {
+        if (!process.env.MONGODB_URI) {
+            throw new Error("MONGODB_URI environment variable is not set");
+        }
+        if (!DB_NAME) {
+            throw new Error("DB_NAME is not defined in constants.js");
+        }
         const connectionStatus = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log("DATAbase Connceted succesfull, HOST :", connectionStatus.connection.host);
+        console.log("Database Connected Successfully, HOST:", connectionStatus.connection.host);
     } catch (error) {
-        console.log("DATABASE CONNECTION ERROR :" , error);
-        process.exit(1) ;       
+        console.error("DATABASE CONNECTION ERROR:", error.message);
+        process.exit(1);
     }
-}
+};
 
 export default dbConnect;
--- 03StreamHub/src/index.js
+++ 03StreamHub/src/index.js
@@ -1,36 +1,12 @@
 import dotenv from "dotenv";
-dotenv.config({
-  path: "./env",
-});
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js";
+dotenv.config();
 import express from "express";
 import dbConnect from "./DB/index.js";
+
+console.log(process.env.MONGODB_URI);
 
 const app = express();
 
 dbConnect();
 
-// effies
-// (async () => {
-//   console.log("started async part");
 
-//   try {
-//     console.log("before await");
-
-//     app.listen(process.env.PORT, ()=>{
-//         console.log(`App is listning on port ${process.env.PORT}`);
-//     })
-
-//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//     console.log("after await");
-//     app.on("error", (error) => {
-//         console.log("ERROR :", error);
-//         // throw error;
-//     })
-
-//   } catch (error) {
-//     console.log("ERROR :", error);
-//     //   throw error;
-//   }
-// })();
