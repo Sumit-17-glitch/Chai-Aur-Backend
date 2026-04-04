260dda279fa03bf3adeb861a5346713f000cdc67
Sumit-17-glitch <skkillgrave@gmail.com>
2026-04-04 13:13:57 +0530

    worked on apiError class

--- 03StreamHub/src/index.js
+++ 03StreamHub/src/index.js
@@ -1,12 +1,20 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+
 import dbConnect from "./DB/index.js";
+import { app } from "./app.js";
 
-console.log(process.env.MONGODB_URI);
-
-const app = express();
-
-dbConnect();
+dbConnect()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listning on port ${process.env.PORT || 8000}`);
+    })
+    app.get("/", (req, res) => {
+        res.send("get succesfull")
+    })
+})
+.catch((e) => {
+    console.log("MONGO db connection error : ", e);
+})
 
 
---
+++ 03StreamHub/src/utils/apiError.js
@@ -0,0 +1,21 @@
+class apiError extends Error{
+    constructor(
+        stauscode,
+        message = "Something went wrong",
+        errors = [],
+        stack = []
+    ){
+        super(message);
+        this.statuscode = stauscode;
+        this.data = null;
+        this.message = message;
+        this.success = false;
+        this.errors = errors;
+
+        if(stack) {
+            this.stack = stack;
+        }else{
+            Error.captureStackTrace(this. this.constructor);
+        }
+    }
+}
